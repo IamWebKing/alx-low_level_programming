945a8b4985322c39c49e62b83a9db6204a17cc2d
IamWebKing <iamtolase@gmail.com>
2023-06-18 06:34:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main -it prints out output using put
  *
- * Return: Always (0) (Success)
+ * Return: Always (0)
  *
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle\n");
         return (0);
 }
