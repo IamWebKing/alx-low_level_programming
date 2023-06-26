cbb3bfe0630c01c24d6b15960fc9f76a0c61dd60
IamWebKing <iamtolase@gmail.com>
2023-06-26 11:12:45 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - print 1 to 100 and fizzbuss
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((1 % 3 == 0) && (1 % 5 == 0))
+		{
+		printf("fizzbuzz");
+		}
+		else if (1 % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (1 % 5 == 0)
+		{
+		printf("Buzz ");
+		}
+		else
+		{
+		printf("%d ", i);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+
+	return (0);
+}
