342700a727a70f09a3647b4e26fb996a88ca7074
IamWebKing <iamtolase@gmail.com>
2023-07-09 15:43:33 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include "main.h"
 
 /**
@@ -69,7 +69,7 @@ return (1);
 
 num1 = _atoi(argv[1]);
 num2 = _atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 
 printf("%d\n", result);
 return (0);
